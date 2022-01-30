172a5ba48fe071c15252acd30bb723c59e1c96fa
Tareq23 <mdtarequlislam634@gmail.com>
2022-01-30 23:54:49 +0600

    add provider state management dependence

---
+++ lib/pages/home.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
