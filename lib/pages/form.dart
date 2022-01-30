2071f1d1a9b7e261ac971e2e3dd892b6cd6d878f
Tareq23 <mdtarequlislam634@gmail.com>
2022-01-31 01:11:08 +0600

    FormPage and TextFormField

---
+++ lib/pages/form.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+class FormPage extends StatefulWidget {
+  const FormPage({Key? key}) : super(key: key);
+
+  @override
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+
+  String? name;
+  String? username;
+  String? email;
+  String? address;
+  String? aboutInfo;
+  var emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 18.0,top: 10),
+            child: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(Icons.arrow_back_ios,color: Colors.grey,size: 35,),
+            ),
+          ),
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  _buildName(),
+                  _buildUsername(),
+                  _buildEmail(),
+                  _buildSortDescription(),
+                  _buildAddress(),
+                  const SizedBox(height: 60,),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    margin: const EdgeInsets.only(right: 60),
+                    child: ElevatedButton(
+                      onPressed: (){
+                        if(!_formKey.currentState!.validate())
+                          {
+                            return ;
+                          }
+                        else if(_formKey.currentState!.validate())
+                          {
+                            _formKey.currentState!.save();
+                            _formKey.currentState!.reset();
+                          }
+                      },
+                      child: Text('Submit'.toUpperCase(),style: const TextStyle(fontSize: 22,fontWeight: FontWeight.w600),),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  Widget _buildName()
+  {
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText:  'Enter Your Name',
+        hintStyle: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),
+      ),
+      validator: (String? value){
+        if(value!.isEmpty)
+          {
+            return "name required";
+          }
+      },
+      onSaved: (value){
+        name = value;
+      },
+    );
+  }
+
+  Widget _buildUsername()
+  {
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText:  'Enter Username',
+        hintStyle: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),
+      ),
+      validator: (value){
+        if(value!.isEmpty)
+          {
+            return "username required";
+          }
+        else if(value.length < 6){
+          return "more than 6 characters";
+        }
+      },
+      onSaved: (value){
+        username = value;
+      },
+    );
+  }
+
+  Widget _buildEmail()
+  {
+    return TextFormField(
+      decoration: const InputDecoration(
+        hintText:  'Email',
+        hintStyle: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),
+      ),
+      validator: (String? value){
+        // bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value!);
+        if(value!.isEmpty)
+        {
+          return "Email required";
+        }
+        else if(!emailValid.hasMatch(value)){
+          return "Invalid Email";
+        }
+      },
+      onSaved: (value){
+        name = value;
+      },
+    );
+  }
+
+  Widget _buildAddress()
+  {
+    return TextFormField(
+      keyboardType: TextInputType.multiline,
+      textInputAction: TextInputAction.newline,
+      minLines: 3,
+      maxLines: 5,
+      decoration: const InputDecoration(
+        hintText:  'Address',
+        hintStyle: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),
+      ),
+      validator: (String? value){
+        if(value!.isEmpty)
+        {
+          return "write address";
+        }
+      },
+      onSaved: (value){
+        name = value;
+      },
+    );
+  }
+
+  Widget _buildSortDescription()
+  {
+    return TextFormField(
+      keyboardType: TextInputType.multiline,
+      textInputAction: TextInputAction.newline,
+      minLines: 3,
+      maxLines: 10,
+      decoration: const InputDecoration(
+        hintText:  'Enter Your Name',
+        hintStyle: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),
+      ),
+      validator: (String? value){
+        if(value!.isEmpty)
+        {
+          return "write anything about her/his";
+        }
+      },
+      onSaved: (value){
+        name = value;
+      },
+    );
+  }
+
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,16 @@
 
 import 'package:flutter/material.dart';
+import 'package:to_do/pages/form.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
       debugShowCheckedModeBanner: false,
       home: HomeApp(),
     );
@@ -23,6 +27,24 @@ class HomeApp extends StatefulWidget {
 class _HomeAppState extends State<HomeApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: const Icon(Icons.menu,color: Colors.blueGrey,size: 40,),
+          onPressed: (){
+
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> const FormPage()));
+        },
+        backgroundColor: Colors.teal,
+        child: const Icon(Icons.add_circle,color: Colors.white,size: 35,),
+      ),
+    );
   }
 }
